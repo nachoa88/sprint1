51b7fd84f99e268617f8dae0359aa4f9ddd7fe05
Nacho Albiol <nacho23@pluralcamp.com>
2024-02-26 18:58:59 +0100

    Added Nivell 1 (exercicis 3 i 4)

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 ### Nivell 1
 1) Print Array element per element.
 2) Manipulació d'arrays, eliminar elements, reordenar.
-3) Funció amb arrays.
-
+3) Torbar una lletra a dins d'una array the paraules.
+4) Creació d'un array associatiu (clau, valor).
 
 
 PENDENTS: Nivell 2, Nivell 3
--- nivell1/arrays.php
+++ nivell1/arrays.php
@@ -28,4 +28,37 @@ echo "And now the size of the array is: " . (sizeof($X)) . "\n";
 
 // Exercici 3: Funció amb arrays.
 echo "--- Exercici 3 --- \n";
+$array_paraules = ["hola", "adeu", "fins aviat", "a reveure", "fins la propera"];
 
+function findLetter(array $array, string $letter)
+{
+    // Inicialitzem la variable que ens indicarà si la lletra es troba a alguna de les paraules per poder tornar true o false.
+    $containLetter = true;
+    // Convertim la lletra a minúscula.
+    $lowercaseLetter = strtolower($letter);
+
+    foreach ($array as $string) {
+        // Convertim la paraula també a minúscula.
+        $lowercaseString = strtolower($string);
+        // Convertim cada string en un array de caràcters.
+        $characters = str_split($lowercaseString);
+        // Si la lletra no es troba en algun dels arrays de caràcters, canviem el valor de la variable a false i sortim del bucle.
+        if (!in_array($lowercaseLetter, $characters)) {
+            $containLetter = false;
+            echo "La lletra " . $letter . " no es troba al menys a una de les paraules.\n";
+            break;
+        }
+    }
+    
+    return $containLetter;
+}
+
+$doesContainLetter = findLetter($array_paraules, "a");
+echo "La lletra es troba a totes les paraules: " . $doesContainLetter;
+
+// Exercici 4: Arrays associatius.
+echo "--- Exercici 4 --- \n";
+
+$my_information = array("nom" => "Nacho", "edat" => 35, "email" => "nacho.albiol88@gmail.com", "menjar favorit" => "milanesa amb puré de patates");
+
+print_r($my_information);
