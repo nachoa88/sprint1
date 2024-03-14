841801f277e1574cea5038b0507bd5397cfba167
Nacho Albiol <nacho23@pluralcamp.com>
2024-03-14 17:16:00 +0100

    Merged tema3 into sprint1

--- nivell1/ex_4.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Exercici 4: Arrays associatius.
-echo "--- Exercici 4 --- \n";
-
-$my_information = array("nom" => "Nacho", "edat" => 35, "email" => "nacho.albiol88@gmail.com", "menjar favorit" => "milanesa amb puré de patates");
-
-print_r($my_information);
