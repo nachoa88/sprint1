844cf9bc2230cd7a0851f7df8013fef58c35c6e1
Nacho Albiol <nacho23@pluralcamp.com>
2024-03-15 16:38:46 +0100

    Update tema7 nivell3 - Added Long Book test

--- tema7/tests/nivell3/LibraryTest.php
+++ tema7/tests/nivell3/LibraryTest.php
@@ -18,16 +18,46 @@ final class LibraryTest extends TestCase
         ];
     }
 
+    public static function providerLibrary(): array
+    {
+        // Creem un array amb tots els llibres.
+        $allBooks1 = [
+            new Book('El Quijote', 'Cervantes', '1234567890', 'Novel·la Policial', 1000),
+            new Book('El Señor de los Anillos', 'Tolkien', '1234567891', 'Fantàstic', 1500),
+        ];
+        // Creem un array amb els llibres que tenen més de 500 pàgines, expected result.
+        $longBooks1 = [
+            new Book('El Quijote', 'Cervantes', '1234567890', 'Novel·la Policial', 1000),
+            new Book('El Señor de los Anillos', 'Tolkien', '1234567891', 'Fantàstic', 1500),
+        ];
+        // Creem una segona array per fer un segon test.
+        $allBooks2 = [
+            new Book('El Perfume', 'Süskind', '1234567892', 'Novel·la Policial', 300),
+            new Book('El Código Da Vinci', 'Brown', '1234567893', 'Novel·la Policial', 600),
+            new Book('El Hobbit', 'Tolkien', '1234567894', 'Fantàstic', 400),
+        ];
+        $longBooks2 = [
+            new Book('El Código Da Vinci', 'Brown', '1234567893', 'Novel·la Policial', 600),
+        ];
+        // Tornem els parells de arrays, allBooks i longBooks(expected result).
+        return [
+            [$allBooks1, $longBooks1],
+            [$allBooks2, $longBooks2],
+        ];
+    }
+
 
     #[DataProvider('providerBook')]
-    public function testAddBook($book){
+    public function testAddBook($book)
+    {
         $library = new Library();
         $library->addBook($book);
         $this->assertEquals($library->getBooks(), [$book]);
     }
 
     #[DataProvider('providerBook')]
-    public function testDeleteBook($book){
+    public function testDeleteBook($book)
+    {
         $library = new Library();
         // Hem de crear un altre llibre different del que ens passen per paràmetre per esborrar uno i que quedi l'altre. 
         $book2 = new Book('Romeo y Julieta', 'Shakespeare', '1234567895', 'Novel·la Policial', 200);
@@ -41,7 +71,8 @@ final class LibraryTest extends TestCase
     }
 
     #[DataProvider('providerBook')]
-    public function testModifyBook($book){
+    public function testModifyBook($book)
+    {
         $library = new Library();
         $library->addBook($book);
         // Clonem el llibre i modifiquem per exemple el titol.
@@ -51,5 +82,12 @@ final class LibraryTest extends TestCase
         $library->modifyBook($modifiedBook);
         // Revisem que el llibre s'hagi modificat.
         $this->assertEquals($library->getBooks(), [$modifiedBook]);
+    }
+
+    #[DataProvider('providerLibrary')]
+    public function testGetLongBooks($allBooks, $expectedBooks)
+    {
+        $library = new Library($allBooks);
+        $this->assertEquals($expectedBooks, $library->getLongBooks());
     }
 }
